e9da8c6cfd808ef9fc07d954fe9bcd13134a3f83
Louis Maddox <louismmx@gmail.com>
2021-01-05 22:15:34 +0000

    Much faster after implementing #8 but rewriting loop logic to permit retrials if no matches found

--- join_hash_tables.py
+++ join_hash_tables.py
@@ -4,6 +4,9 @@ from pathlib import Path
 import pandas as pd
 from imagehash import hex_to_hash
 import warnings
+from operator import and_ as bitwise_and
+from functools import reduce
+from sys import stderr
 
 result_db = "matches.db"
 osx_db = Path("osx/catalina/osx_emoji_hashes.db")
@@ -33,7 +36,7 @@ def write_result_to_db(glyph, glyph_dict):
         for matched_glyph, dist_dict in glyph_dict.items():
             a_hash, c_hash, d_hash = [dist_dict.get(f"{L}_dist") for L in "acd"]
             values = (glyph, matched_glyph, a_hash, c_hash, d_hash, is_multi)
-            c.execute("INSERT INTO top_hash_matches VALUES (?,?,?,?,?)", values)
+            c.execute("INSERT INTO top_hash_matches VALUES (?,?,?,?,?,?)", values)
         conn.commit()
 
 def fetch_sql_query(conn, sql_query, query_variable):
@@ -90,8 +93,15 @@ for i, osx_row in tqdm(osx_df.iterrows(), total=osx_df.shape[0]):
 with open("codepoint_matched_ejp_filenames.txt", "w") as f:
     f.writelines([f"{l}\n" for l in codepoint_matched_ejp_filenames])
 
-for i, osx_row in tqdm(osx_df.iterrows(), total=osx_df.shape[0]):
+n_rows = osx_df.shape[0]
+pbar = tqdm(total=n_rows + 1)
+i = 0
+#for i, osx_row in tqdm(osx_df.iterrows(), total=osx_df.shape[0]):
+while i < n_rows:
+    osx_row = osx_df.iloc[i]
     glyph = osx_row.filename
+    if glyph == "glyph-hiddenglyph.png":
+        continue # not a real glyph, will not get a match, skip it
     if glyph in codepoint_matched_glyphs:
         glyph_dict = codepoint_matched_glyphs.get(glyph)
     else:
@@ -99,7 +109,26 @@ for i, osx_row in tqdm(osx_df.iterrows(), total=osx_df.shape[0]):
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             oah, och, odh = [hex_to_hash(osx_row.get(f"{L}_hash")) for L in "acd"]
-        for i, ejp_row in ejp_df.iterrows():
+        glyph_codepoints = glyph[glyph.find("u"):glyph.find(".")].split("_")
+        if glyph == "glyph-u1F468_u1F3ED.5.png":
+            pass # breakpoint()
+        matchable_codepts = [x.lstrip("u").lower() for x in glyph_codepoints]
+        partial_matchers = [ejp_df.filename.str.contains(x) for x in matchable_codepts]
+        row_filter = reduce(bitwise_and, partial_matchers)
+        if row_filter.any():
+            idx_filter = row_filter
+        else:
+            retry_row_filter = ejp_df.filename.str.contains(matchable_codepts[0])
+            if retry_row_filter.any():
+                idx_filter = retry_row_filter
+                breakpoint()
+            else:
+                if matchable_codepts[0] == "27a1":
+                    breakpoint()
+                print(f"Retry failed with {matchable_codepts[0]}", file=stderr)
+                # Iterate over all rows (the following line is similar to np.ones_like)
+                idx_filter = pd.Series(1, ejp_df.index, dtype="bool")
+        for i, ejp_row in ejp_df[idx_filter].iterrows():
             ejp_filename = ejp_row.filename
             if ejp_filename in codepoint_matched_ejp_filenames:
                 continue # do not score emojipedia icons already matched by codepoint
@@ -117,7 +146,7 @@ for i, osx_row in tqdm(osx_df.iterrows(), total=osx_df.shape[0]):
                 set_to_update = False
                 keys_to_delete = []
                 for gly_k, gly_v in glyph_dict.items():
-                    gly_a, gly_c, gly_d = [gly_v.get("{L}_dist") for L in "acd"]
+                    gly_a, gly_c, gly_d = [gly_v.get(f"{L}_dist") for L in "acd"]
                     if (dist_d, dist_c, dist_a) < (gly_d, gly_c, gly_a):
                         # Delete any (potentially multiple same score) more distant glyphs
                         keys_to_delete.append(gly_k)
@@ -135,4 +164,15 @@ for i, osx_row in tqdm(osx_df.iterrows(), total=osx_df.shape[0]):
             else:
                 dist_dict = {"a_dist": dist_a, "c_dist": dist_c, "d_dist": dist_d}
                 glyph_dict.update({ejp_filename: dist_dict})
-        write_result_to_db(glyph, glyph_dict)
+        if glyph_dict:
+            write_result_to_db(glyph, glyph_dict)
+            retry_with_all_rows = False
+            i += 1
+        elif retry_with_all_rows:
+            # Already retried, give up and just print an error message
+            print(f"Warning: no match was found for '{glyph}'", file=stderr)
+            retry_with_all_rows = False
+            i += 1
+        else:
+            retry_with_all_rows = True
+            # Do not increment i
